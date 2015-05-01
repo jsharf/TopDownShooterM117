109b6fa09d66872bbe1745aa7e4a9ad0fd14046a
Tania DePasquale <rawrtan@gmail.com>
2015-04-30 19:02:19 -0700

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI () {
+		if (GUI.Button (new Rect (Screen.width * 0.25f, Screen.height * 0.25f,
+		                      Screen.width * 0.5f, Screen.height * 0.1f), "Play Game")) {
+			Application.LoadLevel ("game");
+		}
+	}
+}
